e83c7557f8d7e6db7bd9a0a4abb907f1b433fbc1
David McNeary <david@mcneary.tech>
2026-02-10 18:58:32 -0800

    Fix passport session deserialization

--- passport/index.js
+++ passport/index.js
@@ -4,13 +4,14 @@ const User = require('../models/user')
 
 // called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
-	done(null, { _id: user._id })
+	done(null, user._id)
 })
 
 // user object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
+	const userId = id && id._id ? id._id : id
 	User.findOne(
-		{ _id: id },
+		{ _id: userId },
 		'username',
 		(err, user) => {
 			done(null, user)
